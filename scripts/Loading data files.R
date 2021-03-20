d21ddb8c1c13f69a4f0d0f377d78625b1614b598
mario-galindo <mgalindo9410@gmail.com>
2021-03-19 23:26:41 -0600

    lesson loading data files finished

--- scripts/Loading data files.R
+++ scripts/Loading data files.R
@@ -6,3 +6,19 @@ WHO = read.csv("data/WHO.csv")
 
 # See the structured of a data frame
 str(WHO)
+
+# View a summary of data frame
+summary(WHO)
+
+#Create a subset filtering a data frame
+WHO_Europe = subset(WHO, Region == "Europe")
+
+#Save a data frame as CSV file
+write.csv(WHO_Europe,"data/WHO_Europe.csv")
+
+#View a list of current variables
+ls()
+
+#Remove a variable
+rm(WHO_Europe)
+
