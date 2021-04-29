15fc5e4e2eb00d80aec7fe5646f33044622104e2
mario-galindo <mgalindo9410@gmail.com>
2021-04-28 23:16:14 -0600

    adding a new datasets for testing linear regression for predict wine price

---
+++ Unit 2 - Linear Regression/Scripts/Initial Exploration.R
@@ -0,0 +1,10 @@
+# Import Wine Dataset
+wine = read.csv("Unit 2 - Linear Regression/Data/wine.csv")
+
+# View the structure of dataset
+str(wine)
+
+# Get the Average price
+mean(wine$Price)
+ 
+# Equation y = 7.07 
