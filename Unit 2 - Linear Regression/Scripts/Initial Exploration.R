f3fe44624dac82b9b3139ab030f27ea1577d9497
mario-galindo <mgalindo9410@gmail.com>
2021-05-28 14:46:37 -0600

    undestanding the model

--- Unit 2 - Linear Regression/Scripts/Initial Exploration.R
+++ Unit 2 - Linear Regression/Scripts/Initial Exploration.R
@@ -26,12 +26,20 @@ SSE_Model2 = sum(model2$residuals^2)
 SSE_Model2
 
 # Creating a Model3 with all independent variables
-model3 = lm(Price ~ AGST + HarvestRain + WinterRain + FrancePop , data = wine)
+model3 = lm(Price ~ AGST + HarvestRain + WinterRain + FrancePop + Age , data = wine)
 summary(model3)
 
 SSE_Model3 = sum(model3$residuals^2)
 SSE_Model3
 
+#Question #1
+model4_quiz = lm(Price ~ HarvestRain + WinterRain, data = wine)
+summary(model4_quiz)
+
+#Improve the model removing unnecessary independent variables
+model4 = lm(Price ~ AGST + HarvestRain + WinterRain + Age , data = wine)
+summary(model4)
+summary(model3)
 
 
 
